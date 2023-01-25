457ad40948e959b8793ad2533082fd0a6eaa89cb
Arjun Teotia <arjun.a.teotia@infinityworks.com>
2023-01-25 10:27:52 +0000

    fixed some lint

--- app/src/androidTest/kotlin/com/infinity/foodstandards/RatingsFragmentTest.kt
+++ app/src/androidTest/kotlin/com/infinity/foodstandards/RatingsFragmentTest.kt
@@ -1,23 +1,9 @@
 package com.infinity.foodstandards
 
-import android.view.View
-import android.view.ViewGroup
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class RatingsFragmentTest {
-
-}
+class RatingsFragmentTest
